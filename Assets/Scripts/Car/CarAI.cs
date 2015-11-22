a3feec9e866f71848d46758b64e8aa766f96c053
Rodney Rodriguez <rodneyxr@gmail.com>
2015-11-21 20:59:45 -0800

    Implement intersection with lane barricades

--- Assets/Scripts/Car/CarAI.cs
+++ Assets/Scripts/Car/CarAI.cs
@@ -3,27 +3,30 @@ using System.Collections;
 
 public class CarAI : MonoBehaviour
 {
-	public Destinations destinations;
-    public float stopDistance = 1;
+	public float stopDistance = 1;
 	NavMeshAgent car;
 
-	// Use this for initialization
 	void Awake ()
 	{
 		car = GetComponent<NavMeshAgent> ();
 		car.speed = 8f;
 	}
 
-    public void setDestination(Vector3 pos)
-    {
-        car.SetDestination(pos);
-    }
-    void Update ()
-    {
-        if (Vector3.Distance(transform.position, car.destination) < stopDistance)
-        {
-            GameObject.Destroy(gameObject);
-        }
-    }
+	/// <summary>
+	/// Sets the destination for the car.
+	/// </summary>
+	/// <param name="pos">The position of the car's destination.</param>
+	public void setDestination (Vector3 pos)
+	{
+		car.SetDestination (pos);
+	}
+
+	void Update ()
+	{
+		// check if car has reached its destination
+		if (Vector3.Distance (transform.position, car.destination) < stopDistance) {
+			GameObject.Destroy (gameObject);
+		}
+	}
 
 }
--- Assets/Scripts/Dispatcher.cs
+++ Assets/Scripts/Dispatcher.cs
@@ -2,42 +2,49 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Dispatcher : MonoBehaviour {
-    private List<Transform> houses;
-
-    private float delay = 2f;
-    private bool canSpawn = true;
-
-    public GameObject carPrefab;
+public class Dispatcher : MonoBehaviour
+{
+	private List<Transform> houses;
+	public float delay = 2f;
+	private bool canSpawn = true;
+	public GameObject carPrefab;
 	// Use this for initialization
-	void Awake () {
-        houses = new List<Transform>();
-        var tempHouses = GameObject.FindGameObjectsWithTag("House");
-        foreach(GameObject obj in tempHouses)
-        {
-            houses.Add(obj.transform);
-        }
+	void Awake ()
+	{
+		houses = new List<Transform> ();
+		var tempHouses = GameObject.FindGameObjectsWithTag ("House");
+		foreach (GameObject obj in tempHouses) {
+			houses.Add (obj.transform);
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        Spawn();
+	void Update ()
+	{
+		Spawn ();
 	}
 
-    private void Spawn()
-    {
-        if (!canSpawn)
-            return;
-        StartCoroutine(DelaySpawn());
-        var house2spawn = houses[Random.Range(0, houses.Count - 1)];
-        GameObject c = (GameObject)Instantiate(carPrefab, house2spawn.position, house2spawn.rotation);
-        CarAI mycar = c.GetComponent<CarAI>();
-        mycar.setDestination(houses[Random.Range(0, houses.Count - 1)].position);
-    }
-    IEnumerator DelaySpawn()
-    {
-        canSpawn = false;
-        yield return new WaitForSeconds(delay);
-        canSpawn = true;
-    }
+	/// <summary>
+	/// Spawn a car if the timer allows.
+	/// </summary>
+	private void Spawn ()
+	{
+		if (!canSpawn)
+			return;
+		StartCoroutine (DelaySpawn ());
+		var house2spawn = houses [Random.Range (0, houses.Count - 1)];
+		GameObject c = (GameObject)Instantiate (carPrefab, house2spawn.position, house2spawn.rotation);
+		CarAI mycar = c.GetComponent<CarAI> ();
+		mycar.setDestination (houses [Random.Range (0, houses.Count - 1)].position);
+	}
+
+	/// <summary>
+	/// Delays the spawning of vehicles.
+	/// </summary>
+	IEnumerator DelaySpawn ()
+	{
+		canSpawn = false;
+		yield return new WaitForSeconds (delay);
+		canSpawn = true;
+	}
 }
