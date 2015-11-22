9641bbe8c71a504bbd58506ea0c4a0c557e31617
Rodney Rodriguez <rodneyxr@gmail.com>
2015-11-21 18:17:28 -0800

    Add navmesh cars with random destinations

--- Assets/Scripts/Car/CarAI.cs
+++ Assets/Scripts/Car/CarAI.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CarAI : MonoBehaviour {
+public class CarAI : MonoBehaviour
+{
+	public Destinations destinations;
+	NavMeshAgent car;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		car = GetComponent<NavMeshAgent> ();
+		car.speed = 8f;
+		car.SetDestination (destinations.GetRandomDestination().position);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
---
+++ Assets/Scripts/Destinations.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Destinations : MonoBehaviour
+{
+
+	public Transform[] destinations;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		destinations = GetComponentsInChildren<Transform> ();
+	}
+
+	public Transform GetRandomDestination ()
+	{
+		int index = Random.Range (0, destinations.Length - 1);
+		return destinations [index];
+	}
+}
