22e517632e8f9a0fa68d44fd5ee3fac9536ce10d
PySean <smsod18@hotmail.com>
2015-11-21 20:24:22 -0600

    Added missing assets

---
+++ Assets/Scripts/House/CarGenerator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CarGenerator : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        //Just a test (works).
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
